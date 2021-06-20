6fc60805e96384ee41b82233c7ea4d832eb42909
kimsengtan <seng9787@gmail.com>
2021-06-20 14:04:32 +0800

    feat: test for city weather

--- test/CityWeather.spec.js
+++ test/CityWeather.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import CityWeather from '@/components/CityWeather.vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(CityWeather)
     expect(wrapper.vm).toBeTruthy()
   })
 })
